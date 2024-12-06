55ce88d6e52b66b60711e764944883eb32846378
Tasneem Kabir <tzkabir@gmail.com>
2024-12-06 15:20:58 -0500

    Update rp_handler.py
    replaced the return statement as it breaks the for loop

--- src/rp_handler.py
+++ src/rp_handler.py
@@ -299,7 +299,7 @@ def handler(job):
 
     for update_number in range(0, 100):
         runpod.serverless.progress_update(job, f"{update_number}")
-        return {"progress": update_number}
+        time.sleep(1)
 
     # Make sure that the ComfyUI API is available
     check_server(
